43a6c77990c8572d74165af5d17c5da56279dcf8
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-11-16 07:49:37 +0400

    Update ASG only one instance max

--- api/asg.tf
+++ api/asg.tf
@@ -63,7 +63,7 @@ resource "aws_autoscaling_group" "api_asg" {
   name                      = "${var.prefix}-asg"
   launch_configuration      = aws_launch_configuration.api_launch_config.name
   min_size                  = 1
-  max_size                  = 2
+  max_size                  = 1
   desired_capacity          = 1
   health_check_type         = "ELB"
   health_check_grace_period = 300
